98bfabe479a610c9c73f194f298af22573058c92
Mittal <mit.banker1@gmail.com>
2016-05-24 11:39:24 +0530

    Initial Commit

---
+++ IOSDownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSDownloadManager
+//
+//  Created by Mittal J. Banker on 24/05/16.
+//  Copyright © 2016 digicorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSDownloadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSDownloadManager
+//
+//  Created by Mittal J. Banker on 24/05/16.
+//  Copyright © 2016 digicorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
